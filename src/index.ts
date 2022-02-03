d9de35c91b544b6667575c88c0381f952acd386d
ttyago <ttyago.dev@gmail.com>
2022-02-03 15:42:03 +0100

    docs(index.ts): added JSDocs

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,57 @@
 import { InfuraProvider } from '@ethersproject/providers';
-import {formatEther} from'@ethersproject/units';
+import { formatEther } from '@ethersproject/units';
 import { Interface } from '@ethersproject/abi';
-import {Contract} from '@ethersproject/contracts';
+import { Contract } from '@ethersproject/contracts';
 import { OpenseaTransactionInfo, ILogs } from './Interface';
 import axios from 'axios';
-import { config } from 'dotenv-safe';
-config();
 
-const provider = new InfuraProvider(
-  'mainnet',
-  process.env.InfuraProjectID
-);
+const Provider = new InfuraProvider('mainnet', process.env.InfuraProjectID);
 
 const ERC721Abi =
   '[{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"},{"internalType":"uint256","name":"maxNftSupply","type":"uint256"},{"internalType":"uint256","name":"saleStart","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"BAYC_PROVENANCE","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MAX_APES","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"REVEAL_TIMESTAMP","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"apePrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"emergencySetStartingIndexBlock","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"flipSaleState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxApePurchase","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"numberOfTokens","type":"uint256"}],"name":"mintApe","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"reserveApes","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"saleIsActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"provenanceHash","type":"string"}],"name":"setProvenanceHash","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"revealTimeStamp","type":"uint256"}],"name":"setRevealTimestamp","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"setStartingIndex","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"startingIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"startingIndexBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]';
 const ETH = formatEther;
 
-export async function GetABI(EtherscanAPIKEY: string, ContractAddress: string) {
+/**
+ * 
+ * @param EtherscanAPIKEY 
+ * @param ContractAddress 
+ * @returns 
+ * 
+ * Here Etherscan Api returns ABI for contract.
+ * Only verified contract can be load.
+ * If you get problem loading ABI please check GetTransactionData() function to provide your own ABI
+ */
+async function GetABI(EtherscanAPIKEY: string, ContractAddress: string) {
   try {
     const ABI = await axios.get(
       `https://api.etherscan.io/api?module=contract&action=getabi&address=${ContractAddress}&apikey=${EtherscanAPIKEY}`
     );
     return ABI.data.result;
   } catch (error) {
-    console.error(error);
+    console.error(error, 'Something went wrong getting ABI from Etherscan\nCheck if the contract is verified.\nEtherscan API can load only ABI from verified contract source code.');
   }
 }
+/**
+ * 
+ * @param TrxHash 
+ * @param EtherscanAPIKEY 
+ * @param Abi If the contract you try to load from Etherscan API is not verified please provide the abi as string 
+ * @returns
+ * 
+ */
 
-export async function GetTransactionData(TrxHash: string, EtherscanAPIKEY: string): Promise<ILogs[]> {
+export async function GetTransactionData(
+  TrxHash: string,
+  EtherscanAPIKEY: string,
+  Abi?: string
+): Promise<ILogs[]> {
   try {
-    const t_data = await provider.getTransaction(TrxHash);
+    const t_data = await Provider.getTransaction(TrxHash) ;
     const t_logs = (await t_data.wait()).logs.slice(0, 3);
     const Logs: ILogs[] = [];
 
     for (const _ of t_logs) {
-      const abi = await GetABI(EtherscanAPIKEY, _.address);
+      const abi = Abi ?  Abi : await GetABI(EtherscanAPIKEY, _.address);
       const iface = new Interface(abi);
       const log: ILogs = {
         address: _.address,
@@ -46,20 +63,21 @@ export async function GetTransactionData(TrxHash: string, EtherscanAPIKEY: strin
       };
       Logs.push(log);
     }
-    return Logs; 
+    return Logs;
   } catch (error) {
     console.log('Something went wrong getting Transaction.\n', error);
   }
 }
 
+/**
+ * 
+ * @param logs 
+ * @returns 
+ */
 export async function DecodeTransactionInfo(
   logs: ILogs[]
 ): Promise<OpenseaTransactionInfo> {
-  const ContractsERC721 = new Contract(
-    logs[2].address,
-    ERC721Abi,
-    provider
-  );
+  const ContractsERC721 = new Contract(logs[2].address, ERC721Abi, Provider);
 
   const TransactionInfo = logs[0].iface.parseLog({
     data: logs[0].data,
